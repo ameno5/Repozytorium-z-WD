516292c1da23d7509e9487e65391c6fc3bb5ff57
ameno5 <58747653+ameno5@users.noreply.github.com>
2020-03-05 10:13:20 +0100

    Add files via upload

--- zajęcia2.py
+++ zajęcia2.py
@@ -9,3 +9,17 @@ print(list(range(0, -10, -2)))
 
 imie = "Małgorzata"
 print(imie[0::2])
+
+# Zadanie 1
+# zdanie - input("Podaj Zdanie: ")
+# print(zdanie)
+# print(zdanie.count(' '))
+
+# zadanie 2
+import sys
+a = input("Podaj wartość a: ")
+b = input("Podaj wartość b: ")
+wynik = int(a) * int(b)
+s = sys.stdin.readline()
+print("wynik to: " +s)
+sys.stdout.write(s)
