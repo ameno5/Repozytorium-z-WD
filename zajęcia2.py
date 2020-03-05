cecac938bcf0ec618a16b9c222c8d045966e0554
ameno5 <58747653+ameno5@users.noreply.github.com>
2020-03-05 11:12:16 +0100

    Add files via upload

--- zajęcia2.py
+++ zajęcia2.py
@@ -46,3 +46,12 @@ print(imie[0::2])
 # if (liczba % 5 > 0)
 
 # Zadanie 11
+# def diamond(width=5, symbol='o'):
+#    for level in range(1, width+2, 2):
+#        print(f'{symbol*level:^{width}}')
+#    for level in range(width-2, 0, -2):
+#        print(f'{symbol*level:^{width}}')
+
+# diamond(5)
+# diamond(9)
+# diamond(21)
